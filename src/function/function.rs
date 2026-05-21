863e01a55c79a24a4b6f930bcfebf5c80085e7e7
AS7AR <apo2073@outlook.com>
2026-05-21 13:17:51 +0900

    feat: func deriv/inte

--- src/function/function.rs
+++ src/function/function.rs
@@ -10,6 +10,30 @@ impl<F> Func<F> where F: Fn(f64) -> f64, {
     pub fn call(&self, x: f64) -> f64 {
         (self.func)(x)
     }
+
+    pub fn derivative(&self) -> Self {
+        let h = 1e-10;
+        Func::new(|x| {
+            (self.call(x+h)-self.call(x))/h
+        })
+    }
+
+    pub fn integral(&self) -> Self {
+        let f = self.func.clone();
+
+        Func::new(move |x| {
+            let d = 1e10;
+            let dx = x / d;
+
+            let mut sum = 0.0;
+
+            for i in 0.0..=d {
+                sum += f(i * dx) * dx;
+            }
+
+            sum
+        })
+    }
 }
 
 #[cfg(test)]
