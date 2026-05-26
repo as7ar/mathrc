a31b7b408ccf8dbe96d5be81a3c94cfd107c34a5
AS7AR <apo2073@outlook.com>
2026-05-26 14:54:45 +0900

    feat: add Vector

--- src/vector/vector.rs
+++ src/vector/vector.rs
@@ -7,6 +7,42 @@ impl Vector {
     pub fn new(vec: Vec<f64>) -> Self {
         Self { vec }
     }
+
+    pub fn add(&self, other: Self) -> Result<Self, String> {
+        if self.vec.len() != other.vec.len() {
+            return Err("Dimension Mismatched".into());
+        }
+
+        Ok(Self {
+            vec: self.vec.iter()
+                .zip(other.vec.iter())
+                .map(|(a, b)| a + b).collect()
+        })
+    }
+
+    pub fn sub(&self, other: Self) -> Result<Self, String> {
+        if self.vec.len() != other.vec.len() {
+            return Err("Dimension Mismatched".into());
+        }
+
+        Ok(Self {
+            vec: self.vec.iter()
+                .zip(other.vec.iter())
+                .map(|(a, b)| a - b).collect()
+        })
+    }
+
+    pub fn dot(&self, other: Self) -> Result<f64, String> {
+        if self.vec.len()!=other.vec.len() {
+            return Err("Dimension Mismatched".into())
+        }
+
+        let dot = self.vec.iter().zip(other.vec.iter())
+            .map(|(a,b)| a * b)
+            .sum();
+
+        Ok(dot)
+    }
 }
 
 #[cfg(test)]
